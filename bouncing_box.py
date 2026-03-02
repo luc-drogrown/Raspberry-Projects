77359068a2026fc284ffd1c0ad82ba853bbf4f97
luc-drogrown <draganlucian18@gmail.com>
2026-03-02 14:26:24 +0200

    Finished bouncing_box script

--- bouncing_box.py
+++ bouncing_box.py
@@ -6,22 +6,25 @@ import time
 serial = spi(device=0, port=0, gpio_DC=24, gpio_RST=25)
 device = ssd1306(serial)
 
+xdirection = 1
+ydirection = 1
+
 def get_x(oldx):
-    direction = 1
-    if oldx+5+direction == 127:
-        direction = -1
-    elif oldx-direction == 0:
-        direction == 1
-    newx = oldx+direction
+    global xdirection
+    if oldx+5+xdirection == 127:
+        xdirection = -1
+    elif oldx-xdirection == 0:
+        xdirection = 1
+    newx = oldx+xdirection
     return newx
 
 def get_y(oldy):
-    direction = 1
-    if oldy+5+direction == 63:
-        direction = -1
-    elif oldy-direction == 0:
-        direction = 1
-    newy = oldy+direction
+    global ydirection
+    if oldy+5+ydirection == 63:
+        ydirection = -1
+    elif oldy-ydirection == 0:
+        ydirection = 1
+    newy = oldy+ydirection
     return newy
 
 x = 64
@@ -35,7 +38,7 @@ try:
             y = get_y(y)
             draw.rectangle([(x,y), (x+5, y+5)], fill="white", width=2)
             #draw.rectangle([(64, 32), (69, 37)], fill="white", width=2)
-        time.sleep(1)
+        time.sleep(0.05)
 except KeyboardInterrupt:
     device.cleanup()
     print("Clean Exit!")
