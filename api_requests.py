3c9735e498aec5f6f5108fe77b4bbb524cef9762
luc-drogrown <draganlucian18@gmail.com>
2026-03-03 18:26:57 +0200

    API call test

--- api_requests.py
+++ api_requests.py
@@ -1,7 +1,12 @@
 import requests
 
-auth = "P6EiId3_"
-r = requests.get('https://localhost/api/stats/summary&auth={auth}')
-data = r.json()
-print(f"Status: {data['status']}")
-r.text
+api_payload = {"password": "P6EiId3_"}
+server_cert_path = 'tls_ca.crt'
+try:
+
+    r = requests.request("POST",'https://localhost/api/auth', json=api_payload, verify=server_cert_path)
+    data = r.json()
+    print(f"Status: {data['status']}")
+    r.text
+except Exception as e:
+    print(f"Connection failed: {e}")
