629b308a84fa32f87c8f71040d993408006eb5b4
luc-drogrown <draganlucian18@gmail.com>
2026-03-03 19:04:56 +0200

    tried more api call stuff

--- api_requests.py
+++ api_requests.py
@@ -4,7 +4,7 @@ api_payload = {"password": "P6EiId3_"}
 server_cert_path = 'tls_ca.crt'
 try:
 
-    r = requests.request("POST",'https://localhost/api/auth', json=api_payload, verify=server_cert_path)
+    r = requests.request("POST",'http://localhost/api/auth', json=api_payload, verify=server_cert_path)
     data = r.json()
     print(f"Status: {data['status']}")
     r.text
