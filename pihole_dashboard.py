c27f00b4bd5c9ed2befd6e4bfd8b9e8c98d48145
luc-drogrown <draganlucian18@gmail.com>
2026-03-03 21:08:01 +0200

    Added button and more pages to dashboard

--- pihole_dashboard.py
+++ pihole_dashboard.py
@@ -3,10 +3,22 @@ from luma.core.render import canvas
 from luma.oled.device import ssd1306
 import time
 import requests
+from gpiozero import Button
 
 #initialize display
 serial = spi(device=0, port=0, gpio_DC=24, gpio_RST=25)
 device = ssd1306(serial)
+
+button = Button(4)
+
+menu = 0
+
+def button_pressed():
+    global menu
+    if menu<2:
+        menu += 1
+    else:
+        menu = 0
 
 def createSession():
     url = "http://localhost/api/auth"
@@ -26,12 +38,15 @@ def createSession():
     
 def getStats(sid):
     statsUrl = "http://localhost/api/stats/summary"
+    infoUrl = "http://localhost/api/info/system"
     
     if sid != 0:
         header = {"X-FTL-SID": sid}
     
     stats_r = requests.get(statsUrl, headers=header)
+    info_r = requests.get(infoUrl, headers=header)
     stats = stats_r.json()
+    info = info_r.json()
 
     return stats
 
@@ -41,9 +56,21 @@ try:
     while True:
         stats = getStats(sid)
         with canvas(device) as draw:
-            draw.text((5,5), "Pi-Hole Dashboard", fill="white")
-            draw.line([(0,20), (127,20)], fill="white")
-            draw.text((5,25), f"Ads Today: {stats['queries']['blocked']}", fill="white")
+            if menu == 0:
+                draw.text((5, 5), "Pi-Hole: Ads", fill="white")
+                draw.line([(0, 20), (127, 20)], fill="white")
+                draw.text((5, 25), f"Ads Today: {stats['queries']['blocked']}", fill="white")
+                draw.text((5, 45), f"Percent: {stats['queries']['percent_blocked']}%", fill="white")
+            elif menu == 1:
+                draw.text((5, 5), "Pi-Hole: Clients", fill="white")
+                draw.line([(0, 20), (127, 20)], fill="white")
+                draw.text((5, 25), f"Active: {stats['clients']['active']}", fill="white")
+                draw.text((5, 45), f"Total: {stats['clients']['total']}", fill="white")
+            elif menu == 2:
+                draw.text((5, 5), "Pi-Hole: Status", fill="white")
+                draw.line([(0, 20), (127, 20)], fill="white")
+                draw.text((5, 25), f"Uptime: {info['system']['uptime']} s", fill="white")
+                draw.text((5, 45), f"CPU: {info['system']['cpu']['%cpu']} %", fill="white")
         time.sleep(5)
 except KeyboardInterrupt:
     device.cleanup()
