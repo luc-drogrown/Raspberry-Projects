9fa3b5a6f35cd96222f8922d29794b2e185f6d32
luc-drogrown <draganlucian18@gmail.com>
2026-03-03 18:14:20 +0200

    API call test

--- api_requests.py
+++ api_requests.py
@@ -1,4 +1,7 @@
 import requests
 
-r = requests.get('https://pi.hole/api/stats/summary')
+auth = "P6EiId3_"
+r = requests.get('https://localhost/api/stats/summary&auth={auth}')
+data = r.json()
+print(f"Status: {data['status']}")
 r.text
